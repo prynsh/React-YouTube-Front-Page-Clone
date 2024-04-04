f0cccbd36ef9cfccce98dbbff2735f23e583a70c
Priyansh Verma <priyansh2112@gmail.com>
2024-04-04 13:00:00 +0530

    First CheckPoint{Created the Navbar}

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PageHeader } from "./components/PageHeader";
+
+export default function App() {
+  return <div className="max-h-screen flex flex-col">
+    <PageHeader/>
+    <div>2</div>
+
+  </div>
+}
